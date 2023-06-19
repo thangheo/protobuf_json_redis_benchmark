09921ce14800bf19deec4ea2218edaebfae76e30
Thang Pham <122386703+thangheo@users.noreply.github.com>
2023-06-19 18:33:40 +0700

    Update DataStorage_protobuf.cpp
    config redis port to default 6379

--- src/DataStorage_protobuf.cpp
+++ src/DataStorage_protobuf.cpp
@@ -3,15 +3,12 @@
 #include "protobuf/DataStorage.pb.h"
 
 using namespace std;
-#define DS_RD_PORT 6380
+#define DS_RD_PORT 6379
 DataStorage::DataStorage()
-    // : redisDB("tcp://localhost")
 {
-    // Initialize the RedisPlusPlus connection
     // Replace "tcp://localhost" with the appropriate Redis server address if necessary
     redisDB = std::make_shared<RedisDatabase>("127.0.0.1",DS_RD_PORT);
-    // Set the key prefix
-    keyPrefix = "analytical_data:";
+    keyPrefix = "fruit_protobuf_data:";
 }
 DataStorage::~DataStorage()
 {
@@ -30,20 +27,15 @@ bool DataStorage::addData(const DataStorage_t& data)
 std::vector<DataStorage_t> DataStorage::getData(const std::string& fruit) const
 {   
     
-    // Generate the Redis key
     std::string key = keyPrefix + fruit;
 
-    // Retrieve the value from Redis
     std::optional<std::string> dataStr = redisDB->get(key);
     if (!dataStr)
     {
-        // Key not found, return empty vector
         return std::vector<DataStorage_t>();
     }
-    // Deserialize the data from the string
     DataStorage_t data = deserializeData(*dataStr);
 
-    // Return a vector containing the retrieved data
     return std::vector<DataStorage_t>{data};
 }
 
@@ -63,7 +55,6 @@ DataStorage_t DataStorage::deserializeData(const std::string& dataStr) const
     DataStorage_t data;
 
     if (!data.ParseFromString(dataStr)) {
-        // Invalid protobuf data, return a default-initialized DataStorage
         return DataStorage_t();
     }
 
